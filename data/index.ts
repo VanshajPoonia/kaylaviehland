ec02e44fb8d690329a70976b162ddc73a06b6e8b
Vanshaj Poonia <Vanshajtheunique@gmail.com>
2024-10-04 17:39:20 +0530

    Initial Commit

---
+++ data/index.ts
@@ -0,0 +1,211 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Work", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I prioritize critical thinking, fostering open communication with a blend of ethics",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My skills set",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title:
+      "Senior Accountant with deep knowledge in Microsoft Tools, passports, Data-Entry and much more",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently working at EmpowerMe Wellness",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    // img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Are you hiring or maybe just want get to know each other?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "The Retina Institute",
+    des: "Reconciled all end of month reporting for all businesses as well as bank statements.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://www.mercy.net/practice/mercy-clinic-childrens-heart-center/",
+  },
+  {
+    id: 2,
+    title: "Mercy Clinic Children’s Heart Center",
+    des: "Provided exceptional customer service while investigating, evaluating, and resolving medical claims",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://www.tri-stl.com/",
+  },
+  {
+    id: 3,
+    title: "EmpowerMe Wellness",
+    des: "Senior Accountant.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://empowerme.com/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Beyond their technical skills, Kayla is a team player who communicates clearly and works seamlessly with colleagues from various departments. Their reliability and consistency have earned them the trust and respect of both coworkers and leadership.",
+    name: "Vanshaj Poonia",
+    title: "Founder at Lexter Technologies",
+  },
+  {
+    quote:
+      "Working with Kayla has been an absolute pleasure. Their expertise in accountancy and attention to detail have made an invaluable contribution to our financial operations. They consistently demonstrate a thorough understanding of complex financial processes, ensuring accuracy in all transactions and reports.",
+    name: "Nathan",
+    title: "COO at Hypnosys",
+  },
+  {
+    quote:
+      "We highly recommend Kayla and greatly appreciate the positive impact they've had on our company.",
+    name: "Elara Finch",
+    title: "Director of BriskMedia",
+  },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+];
+
+// export const companies = [
+//   {
+//     id: 1,
+//     name: "cloudinary",
+//     img: "/cloud.svg",
+//     nameImg: "/cloudName.svg",
+//   },
+//   {
+//     id: 2,
+//     name: "appwrite",
+//     img: "/app.svg",
+//     nameImg: "/appName.svg",
+//   },
+//   {
+//     id: 3,
+//     name: "HOSTINGER",
+//     img: "/host.svg",
+//     nameImg: "/hostName.svg",
+//   },
+//   {
+//     id: 4,
+//     name: "stream",
+//     img: "/s.svg",
+//     nameImg: "/streamName.svg",
+//   },
+//   {
+//     id: 5,
+//     name: "docker.",
+//     img: "/dock.svg",
+//     nameImg: "/dockerName.svg",
+//   },
+// ];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Senior Accountant",
+    desc: "Managed finances and economic operations",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Admin Accountant",
+    desc: "Reconciled all end of month reporting for all businesses as well as bank statements.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Billing Coordinator",
+    desc: "Provided exceptional customer service while investigating, evaluating, and resolving medical claims",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Member of Business club",
+    desc: "Delta Mu Delta-International Honor Society in Business",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+// export const socialMedia = [
+//   {
+//     id: 1,
+//     img: "/git.svg",
+//   },
+//   {
+//     id: 2,
+//     img: "/twit.svg",
+//   },
+//   {
+//     id: 3,
+//     img: "/link.svg",
+//   },
+// ];
